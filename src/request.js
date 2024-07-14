760c4de2743c749f0cfcd4905400dac26de233d9
Ilya Zelnikov <asv67@seznam.cz>
2024-07-14 22:55:54 +0200

    fix

--- src/request.js
+++ src/request.js
@@ -5,7 +5,7 @@ export default (state, url, id, watchedState, newfeed = false) => {
   fetch(`https://allorigins.hexlet.app/get?url=${url}`, { cache: "no-cache" })
   .then(response => {
     if (response.ok) return response.json();
-    throw new Error('network error');
+    throw new Error();
   })
   .then(data => {
     if (data.contents.slice(2, 5)=== 'xml') {  
@@ -19,5 +19,5 @@ export default (state, url, id, watchedState, newfeed = false) => {
     state.request.errors = '';
     watchedState.response.status = 'received';
   })
-  .catch(e => {console.log('request23', e); watchedState.request.errors = e.message;});
+  .catch(e => watchedState.request.errors = e.message);
 };
