663a5b1313e8431d943e3935392aa6355c42ca96
Ilya Zelnikov <asv67@seznam.cz>
2024-07-18 15:18:07 +0200

    fix: update render

--- src/render.js
+++ src/render.js
@@ -1,7 +1,61 @@
 import i18next from 'i18next';
-import appFeeds from './appFeeds';
 
-export default (state) => {
+const deleteFeed = (state, id) => {
+  const { feeds, repliesURLs } = state;
+  const { url } = feeds[id];
+  clearTimeout(state.feeds[id].timer);
+  delete feeds[id];
+  state.repliesURLs = repliesURLs.filter(({ urlFeed }) => urlFeed !== url);
+  state.feedBackMessage = 'deleted';
+};
+
+const openPost = (state, id) => {
+  const idt = id.split('.').map((i) => +i);
+  const currentFeed = state.feeds[idt[0]];
+  const [{ post }] = currentFeed.content.items.filter((i) => i.ids === id);
+  post.title.class = 'fw-normal, link-secondary';
+  const {
+    title,
+    description,
+    creator,
+    link,
+  } = post;
+  const modalHeader = document.getElementById('modalHeader');
+  modalHeader.textContent = title.text;
+  const modalBody = document.getElementById('modalBody');
+  modalBody.innerHTML = '';
+  const modalRedirect = document.getElementById('redirect');
+  modalRedirect.firstChild.href = link;
+  const aDescription = document.createElement('a');
+  aDescription.innerHTML = description;
+  const pCreator = document.createElement('p');
+  pCreator.textContent = creator;
+  modalBody.append(aDescription, pCreator);
+};
+
+const render = (state) => {
+
+  const appFeeds = () => {
+    const buttons = document.querySelectorAll('button');
+    buttons.forEach((elButton) => {
+      elButton.addEventListener('click', (e) => {
+        const { action } = e.target.dataset;
+        const { id } = e.target.dataset;
+        switch (action) {
+          case 'deleteFeed':
+            deleteFeed(state, id);
+            render(state);
+            break;
+          case 'openPost':
+            openPost(state, id);
+            break;
+          default:
+            break;
+        }
+      });
+    });
+  };
+ 
   const { feeds, repliesURLs } = state;
   const section = document.getElementById('container-xxl');
   const pFeedBack = document.getElementById('feedback');
@@ -107,3 +161,5 @@ export default (state) => {
     section.innerHTML = '';
   }
 };
+
+export default render;
