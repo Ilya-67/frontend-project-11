cd1f2941d05ddc526c9af6d2ebb567c64b99f4d0
Ilya Zelnikov <asv67@seznam.cz>
2024-07-30 23:11:51 +0200

    fix

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,9 @@
 // import globals from 'globals';
-const globals = require('globals');
+// const globals = require('globals');
 // import path from 'path';
-//const path = require('path');
+// const path = require('path');
 // import { fileURLToPath } from 'url';
-//const { fileURLToPath } = require('node:url');
+// const { fileURLToPath } = require('node:url');
 // import { FlatCompat } from '@eslint/eslintrc';
 const { FlatCompat } = require('@eslint/eslintrc');
 // import pluginJs from '@eslint/js';
@@ -12,11 +12,13 @@ const js = require('@eslint/js');
 //const filename = fileURLToPath('');
 //const dirname = path.dirname(filename);
 const compat = new FlatCompat({
-  baseDirectory: __dirname, recommendedConfig: js.configs.recommended,
+  baseDirectory: __dirname,
+  resolvePluginsRelativeTo: __dirname,
+  recommendedConfig: js.configs.recommended,
 });
 
-module.exports = () => { [
-  { files: ['**/*.js'], languageOptions: { sourceType: 'script' } },
-  { languageOptions: { globals: globals.node } },
+module.exports = [
+  // { files: ['**/*.js'], languageOptions: { sourceType: 'script' } },
+  // { languageOptions: { globals: globals.node } },
   ...compat.extends('airbnb'),
-] };
+];
