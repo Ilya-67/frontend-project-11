e5897135a030f7843b6d115f195919f1dacf0b5c
Ilya Zelnikov <asv67@seznam.cz>
2024-07-30 12:50:04 +0200

    fix: update webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@
 
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
+// const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
 
 const isProduction = process.env.NODE_ENV === 'production';
 
@@ -53,7 +53,7 @@ const config = {
 module.exports = () => {
   if (isProduction) {
     config.mode = 'production';
-    config.plugins.push(new WorkboxWebpackPlugin.GenerateSW());
+    // config.plugins.push(new WorkboxWebpackPlugin.GenerateSW());
   } else {
     config.mode = 'development';
   }
